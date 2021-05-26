2b4aa61d057013f673b672bfbd582b0b07c9c9a4
Varun Mamtora <varunmamtora@gmail.com>
2021-05-26 12:44:45 +0530

    checks for if profile exists

--- fern/main/views/userprofile.py
+++ fern/main/views/userprofile.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from django.core.exceptions import ObjectDoesNotExist
+
 from ..models import *
 
 def profile(request):
-    profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+        Profile.objects.create(phone=request.POST['contact'], address=request.POST['address'], user=request.user)
+        return redirect('profile')
+
+    else:
+        profile = None
+
+        try:
+            profile = Profile.objects.get(user=request.user)
+        except ObjectDoesNotExist:
+            messages.info(request,'Your profile dosent exist.')
+    
     return render(request,'profile.html',{"profile":profile})    
